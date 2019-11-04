bb5d9c760b7c51c1930a0d3717838957995cf854
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 16:23:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   elsif phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
