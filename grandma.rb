d39e5eb3a7847134b1694f6c760e04412830ddf7
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 16:23:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,10 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.uppercase
+    puts "NO, NOT SINCE 1938!"
   end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
