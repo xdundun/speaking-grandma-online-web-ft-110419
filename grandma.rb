cd85ae9fd966de5e3e96b792ff0a427201fffec3
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 16:26:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif !phrase.uppercase
+  elsif phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.uppercase
     return "NO, NOT SINCE 1938!"
